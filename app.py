783b6832e889e6cc935bad2349d3415cacfb6b17
hadiasalman <hadiasalman444@gmail.com>
2026-09-20 07:42:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ import streamlit as st
 import sqlite3
 
 st.title("StudySphere")
-
-def get_connection():
 conn = sqlite3.connect("studysphere.db")
-return conn
-
 st.success("StudySphere is working!")
