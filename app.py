86d3943b6eb9fe4f2b5f088f37c1a0b92bef5f19
hadiasalman <hadiasalman444@gmail.com>
2026-09-20 07:20:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import os
 from datetime import datetime, date, timedelta
 
-# ============================================================
+# ===========================================================
 # STUDYSPHERE — AI STUDENT COMPANION
 # Single-file Streamlit MVP
 # ============================================================
